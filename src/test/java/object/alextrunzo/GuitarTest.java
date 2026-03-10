c656dc9e44890da2732291fabe533b9db00f9bec
atrunz <atrunzo261@gmail.com>
2026-03-10 16:08:56 -0400

    achieved 80% test coverage

--- src/test/java/object/alextrunzo/GuitarTest.java
+++ src/test/java/object/alextrunzo/GuitarTest.java
@@ -1,187 +1,282 @@
 package object.alextrunzo;
 
-import object.alextrunzo.Guitar;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class GuitarTest {
-        @Test
-        public void constructorShouldCreateGuitarWithValidData() {
-
-            Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
-                    true, 24, 24, true,
-                    "E Standard");
-
-            assertEquals("Gibson", guitar.getBrand());
-            assertEquals("Les Paul", guitar.getModel());
-            assertEquals("Jumbo", guitar.getColor());
-            assertTrue(guitar.isElectric());
-            assertEquals(24, guitar.getPrice());
-            assertEquals("E Standard", guitar.getTuning());
-            assertTrue(guitar.isPluggedIn());
-            assertEquals(0, guitar.getVolumeKnobSetting());
-
-
-
-        }
 
     @Test
-        public void setBrandShouldUpdateBrand() {
+    public void constructorShouldCreateGuitarWithValidData() {
         Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
-                true, 24, 24, true,
-                "E Standard");
-            guitar.setBrand("Fender");
-            assertEquals("Fender", guitar.getBrand());
-        }
+                true, 24, 24, true, "E Standard");
+
+        assertEquals("Gibson", guitar.getBrand());
+        assertEquals("Les Paul", guitar.getModel());
+        assertEquals("Jumbo", guitar.getColor());
+        assertTrue(guitar.isElectric());
+        assertEquals(24, guitar.getNumberOfFrets());
+        assertEquals(24, guitar.getPrice());
+        assertTrue(guitar.isPluggedIn());
+        assertEquals(0, guitar.getVolumeKnobSetting());
+        assertEquals("E Standard", guitar.getTuning());
+    }
+
+    @Test
+    public void setBrandShouldUpdateBrand() {
+        Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
+                true, 24, 24, true, "E Standard");
+
+        guitar.setBrand("Fender");
+
+        assertEquals("Fender", guitar.getBrand());
+    }
 
     @Test
     public void setModelShouldUpdateModel() {
         Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
-                true, 24, 24, true,
-                "E Standard");
-        guitar.setModel("Stratocastor");
-        assertEquals("Stratocastor", guitar.getModel());
+                true, 24, 24, true, "E Standard");
+
+        guitar.setModel("Stratocaster");
+
+        assertEquals("Stratocaster", guitar.getModel());
     }
 
     @Test
     public void setColorShouldUpdateColor() {
         Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
-                true, 24, 24, true,
-                "E Standard");
+                true, 24, 24, true, "E Standard");
+
         guitar.setColor("Green");
+
         assertEquals("Green", guitar.getColor());
     }
 
     @Test
-    public void setIsElectricShouldUpdateIsElectric() {
+    public void setElectricShouldUpdateElectricField() {
         Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
-                true, 24, 24, true,
-                "E Standard");
-        guitar.setElectric(false);
-        assertEquals(false, guitar.isElectric());
-    }
+                true, 24, 24, true, "E Standard");
 
+        guitar.setElectric(false);
+
+        assertFalse(guitar.isElectric());
+    }
 
     @Test
     public void setNumberOfFretsShouldUpdateNumberOfFrets() {
         Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
-                true, 24, 24, true,
-                "E Standard");
+                true, 24, 24, true, "E Standard");
+
         guitar.setNumberOfFrets(22.5);
+
         assertEquals(22.5, guitar.getNumberOfFrets());
     }
-
 
     @Test
     public void setPriceShouldUpdatePrice() {
         Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
-                true, 24, 24, true,
-                "E Standard");
+                true, 24, 24, true, "E Standard");
+
         guitar.setPrice(225);
+
         assertEquals(225, guitar.getPrice());
     }
 
     @Test
-    public void setIsPluggedInShouldUpdateIsPluggedIn() {
+    public void setPluggedInShouldUpdatePluggedInField() {
         Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
-                true, 24, 24, true,
-                "E Standard");
+                true, 24, 24, true, "E Standard");
+
         guitar.setPluggedIn(false);
-        assertEquals(false, guitar.isPluggedIn());
+
+        assertFalse(guitar.isPluggedIn());
     }
 
     @Test
-    public void setTuningShouldUpdateTuning() {
+    public void setVolumeKnobSettingShouldUpdateVolumeKnobSetting() {
         Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
-                true, 24, 24, true,
-                "E Standard");
+                true, 24, 24, true, "E Standard");
+
+        guitar.setVolumeKnobSetting(3);
+
+        assertEquals(3, guitar.getVolumeKnobSetting());
+    }
+
+    @Test
+    public void tuneShouldUpdateTuning() {
+        Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
+                true, 24, 24, true, "E Standard");
+
         guitar.tune("Drop D");
+
         assertEquals("Drop D", guitar.getTuning());
     }
 
+    @Test
+    public void playShouldPrintPlayingMessage() {
+        Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
+                true, 24, 24, true, "E Standard");
 
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(output));
 
-//
-//        @Test
-//        public void constructorWithNegativeAgeShouldThrowException() {
-//            assertThrows(
-//                    IllegalArgumentException.class,
-//                    () -> new Person("Jane", "Smith", -1,
-//                            "jane@email.com", "555-1234")
-//            );
-//        }
-//
-//        @Test
-//        public void setFirstNameShouldUpdateFirstName() {
-//            Person person = new Person("Jane", "Smith", 25,
-//                    "jane@email.com", "555-1234");
-//            person.setFirstName("Jordan");
-//            assertEquals("Jordan", person.getFirstName());
-//        }
-//
-//        @Test
-//        public void setLastNameShouldUpdateLastName() {
-//            Person person = new Person("Jane", "Smith", 25,
-//                    "jane@email.com", "555-1234");
-//            person.setLastName("Eldridge");
-//            assertEquals("Eldridge", person.getLastName());
-//        }
-//
-//        @Test
-//        public void setAgeShouldUpdateAge() {
-//            Person person = new Person("Jane", "Smith", 25,
-//                    "jane@email.com", "555-1234");
-//            person.setAge(30);
-//            assertEquals(30, person.getAge());
-//        }
-//
-//        @Test
-//        public void setAgeWithNegativeValueShouldThrowException() {
-//            Person person = new Person("Jane", "Smith", 25,
-//                    "jane@email.com", "555-1234");
-//            assertThrows(
-//                    IllegalArgumentException.class,
-//                    () -> person.setAge(-1)
-//            );
-//        }
-//
-//        @Test
-//        public void setEmailShouldUpdateEmail() {
-//            Person person = new Person("Jane", "Smith", 25,
-//                    "jane@email.com", "555-1234");
-//            person.setEmail("jordan@email.com");
-//            assertEquals("jordan@email.com", person.getEmail());
-//        }
-//
-//        @Test
-//        public void setPhoneNumberShouldUpdatePhoneNumber() {
-//            Person person = new Person("Jane", "Smith", 25,
-//                    "jane@email.com", "555-1234");
-//            person.setPhoneNumber("555-5678");
-//            assertEquals("555-5678", person.getPhoneNumber());
-//        }
-//
-//        @Test
-//        public void getFullNameShouldReturnFirstAndLastName() {
-//            Person person = new Person("Jane", "Smith", 25,
-//                    "jane@email.com", "555-1234");
-//            assertEquals("Jane Smith", person.getFullName());
-//        }
-//
-//        @Test
-//        public void isAdultShouldReturnTrueWhenAgeIsEighteenOrOver() {
-//            Person person = new Person("Jane", "Smith", 18,
-//                    "jane@email.com", "555-1234");
-//            assertTrue(person.isAdult());
-//        }
-//
-//        @Test
-//        public void isAdultShouldReturnFalseWhenAgeIsUnderEighteen() {
-//            Person person = new Person("Jane", "Smith", 17,
-//                    "jane@email.com", "555-1234");
-//            assertFalse(person.isAdult());
-//        }
+        try {
+            guitar.play();
+        } finally {
+            System.setOut(originalOut);
+        }
 
+        assertTrue(output.toString().contains("You are playing the Gibson Les Paul"));
+    }
+
+    @Test
+    public void plugInShouldPrintPlugInMessage() {
+        Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
+                true, 24, 24, false, "E Standard");
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(output));
+
+        try {
+            guitar.plugIn();
+        } finally {
+            System.setOut(originalOut);
+        }
+
+        assertTrue(output.toString().contains("You are pluging the Gibson Les Paulin"));
+    }
+
+    @Test
+    public void setVolumeShouldTurnVolumeUpWhenInputIsHigher() {
+        Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
+                true, 24, 24, true, "E Standard");
+
+        InputStream originalIn = System.in;
+        PrintStream originalOut = System.out;
+        ByteArrayInputStream input = new ByteArrayInputStream("5\n".getBytes());
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        System.setIn(input);
+        System.setOut(new PrintStream(output));
+
+        try {
+            guitar.setVolume();
+        } finally {
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+        }
+
+        assertEquals(5, guitar.getVolumeKnobSetting());
+        assertTrue(output.toString().contains("Enter your new volume"));
+        assertTrue(output.toString().contains("You turned up your volume to 5"));
+    }
+
+    @Test
+    public void setVolumeShouldTurnVolumeDownWhenInputIsLower() {
+        Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
+                true, 24, 24, true, "E Standard");
+        guitar.setVolumeKnobSetting(7);
+
+        InputStream originalIn = System.in;
+        PrintStream originalOut = System.out;
+        ByteArrayInputStream input = new ByteArrayInputStream("3\n".getBytes());
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        System.setIn(input);
+        System.setOut(new PrintStream(output));
+
+        try {
+            guitar.setVolume();
+        } finally {
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+        }
+
+        assertEquals(3, guitar.getVolumeKnobSetting());
+        assertTrue(output.toString().contains("Enter your new volume"));
+        assertTrue(output.toString().contains("You turned down your volume to 3"));
+    }
+
+    @Test
+    public void setVolumeShouldRejectEqualVolume() {
+        Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
+                true, 24, 24, true, "E Standard");
+        guitar.setVolumeKnobSetting(4);
+
+        InputStream originalIn = System.in;
+        PrintStream originalOut = System.out;
+        ByteArrayInputStream input = new ByteArrayInputStream("4\n".getBytes());
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        System.setIn(input);
+        System.setOut(new PrintStream(output));
+
+        try {
+            guitar.setVolume();
+        } finally {
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+        }
+
+        assertEquals(4, guitar.getVolumeKnobSetting());
+        assertTrue(output.toString().contains("Enter your new volume"));
+        assertTrue(output.toString().contains("Sorry bud, your volume doesn't go that far"));
+    }
+
+    @Test
+    public void setVolumeShouldRejectInputAboveTen() {
+        Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
+                true, 24, 24, true, "E Standard");
+
+        InputStream originalIn = System.in;
+        PrintStream originalOut = System.out;
+        ByteArrayInputStream input = new ByteArrayInputStream("11\n".getBytes());
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        System.setIn(input);
+        System.setOut(new PrintStream(output));
+
+        try {
+            guitar.setVolume();
+        } finally {
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+        }
+
+        assertEquals(0, guitar.getVolumeKnobSetting());
+        assertTrue(output.toString().contains("Enter your new volume"));
+        assertTrue(output.toString().contains("Sorry bud, your volume doesn't go that far"));
+    }
+
+    @Test
+    public void setVolumeShouldRejectNegativeInput() {
+        Guitar guitar = new Guitar("Gibson", "Les Paul", "Jumbo",
+                true, 24, 24, true, "E Standard");
+
+        InputStream originalIn = System.in;
+        PrintStream originalOut = System.out;
+        ByteArrayInputStream input = new ByteArrayInputStream("-1\n".getBytes());
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        System.setIn(input);
+        System.setOut(new PrintStream(output));
+
+        try {
+            guitar.setVolume();
+        } finally {
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+        }
+
+        assertEquals(0, guitar.getVolumeKnobSetting());
+        assertTrue(output.toString().contains("Enter your new volume"));
+        assertTrue(output.toString().contains("Sorry bud, your volume doesn't go that far"));
+    }
 }
